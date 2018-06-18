c3dfd67319a3c0c77b3a22298c30872e67f38a41
Edgard Barquero <ebarquero85@gmail.com>
2018-06-18 17:54:40 -0600

    Se agrega el composer autoload

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,12 +4,6 @@ $app = [];
 
 $app['config'] = require 'config.php';
 
-
-require 'Request.php';
-require 'Router.php';
-require 'database/Connection.php';
-require 'database/QueryBuilder.php';
-
 $app['database'] = new QueryBuilder(
     Connection::make($app['config']['database'])
 );
--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
-
 ini_set('display_errors', '1');
+
+require 'vendor/autoload.php';
 
 $database = require 'core/bootstrap.php';
 
-
-require Router::load('routes.php')->direct(Request::url(), Request::method());
+require Router::load('routes.php')->direct(
+    Request::url(),
+    Request::method()
+);
 
 
 
